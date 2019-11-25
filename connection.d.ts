ea5388bec709ddb1b5de854c8468677d0c3e5b67
Rui <rui.d.figueiredo@gmail.com>
2019-11-25 22:51:18 +0000

    Added missing description text

--- connection.d.ts
+++ connection.d.ts
@@ -5,7 +5,7 @@ export = class Connection {
     /**
      * Sends a request and arguments to the connected process
      * @param type The request type
-     * @param args Argument to pass to 
+     * @param args Argument to be sent to the connected process request handler 
      * @param onResponse Optional callback that will be invoked after the request has been executed on the connected process 
      */
     send(type: string, args: any, onResponse?: (returnArg: any) => void): void;
